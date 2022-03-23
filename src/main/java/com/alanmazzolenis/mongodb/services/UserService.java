b74933f672af02249ac5c3cdea31ea8777ce4636
Alan Mazzolenis <alangalvaogg@gmail.com>
2022-03-23 15:26:33 -0300

    Connecting at MongoDB with repository and service

---
+++ src/main/java/com/alanmazzolenis/mongodb/services/UserService.java
@@ -0,0 +1,21 @@
+package com.alanmazzolenis.mongodb.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alanmazzolenis.mongodb.domain.User;
+import com.alanmazzolenis.mongodb.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+
+}
