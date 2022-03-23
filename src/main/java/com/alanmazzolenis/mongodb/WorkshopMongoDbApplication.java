c5f6cd0f4abdb514875da0c96e07a1998ee33b89
Alan Mazzolenis <alangalvaogg@gmail.com>
2022-03-23 11:43:12 -0300

    First commit - project created

---
+++ src/main/java/com/alanmazzolenis/mongodb/WorkshopMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.alanmazzolenis.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alanmazzolenis/mongodb/WorkshopMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanmazzolenis.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
