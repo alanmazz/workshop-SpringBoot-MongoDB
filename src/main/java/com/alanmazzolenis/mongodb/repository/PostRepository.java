4bfed57883e93ac519c7e01ad4d81e467c335850
Alan Mazzolenis <alangalvaogg@gmail.com>
2022-03-25 18:24:59 -0300

    Query with multiple criteria

--- src/main/java/com/alanmazzolenis/mongodb/repository/PostRepository.java
+++ src/main/java/com/alanmazzolenis/mongodb/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.alanmazzolenis.mongodb.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -12,5 +13,9 @@ import com.alanmazzolenis.mongodb.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> findByTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: { $gte: ?1} }, { date: { $lte: ?2} }, {$or: [ { 'title': {$regex: ?0, $options: 'i' }},{'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> SearchAll(String text, Date minDate, Date MaxDate);
 }
--- src/main/java/com/alanmazzolenis/mongodb/resources/PostResource.java
+++ src/main/java/com/alanmazzolenis/mongodb/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.alanmazzolenis.mongodb.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,21 @@ public class PostResource {
 	}
 	
 	@GetMapping(value = "/titlesearch")
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue="") String text) {
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue="") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> findByTitle(
+			@RequestParam(value = "text", defaultValue="") String text,
+			@RequestParam(value = "minDate", defaultValue="") String minDate,
+			@RequestParam(value = "maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
