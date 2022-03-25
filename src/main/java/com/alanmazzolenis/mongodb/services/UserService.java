d8594ac4a9abac4554bf155926dcfc6375a7cc9d
Alan Mazzolenis <alangalvaogg@gmail.com>
2022-03-24 21:19:39 -0300

    User insert with POST

--- src/main/java/com/alanmazzolenis/mongodb/services/UserService.java
+++ src/main/java/com/alanmazzolenis/mongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alanmazzolenis.mongodb.domain.User;
+import com.alanmazzolenis.mongodb.dto.UserDTO;
 import com.alanmazzolenis.mongodb.repository.UserRepository;
 import com.alanmazzolenis.mongodb.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
