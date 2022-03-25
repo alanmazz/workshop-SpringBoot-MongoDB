f6239dc4c25517709c33dc35215030cc77fc99e3
Alan Mazzolenis <alangalvaogg@gmail.com>
2022-03-25 11:21:19 -0300

    Getting post by id

---
+++ src/main/java/com/alanmazzolenis/mongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.alanmazzolenis.mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alanmazzolenis.mongodb.domain.Post;
+import com.alanmazzolenis.mongodb.repository.PostRepository;
+import com.alanmazzolenis.mongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
