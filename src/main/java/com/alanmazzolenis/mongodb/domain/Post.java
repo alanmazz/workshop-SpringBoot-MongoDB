3accfab194cf9c9f4f2f7928a828314d2a977adc
Alan Mazzolenis <alangalvaogg@gmail.com>
2022-03-25 11:39:52 -0300

    Adding comments to posts

--- src/main/java/com/alanmazzolenis/mongodb/domain/Post.java
+++ src/main/java/com/alanmazzolenis/mongodb/domain/Post.java
@@ -1,13 +1,16 @@
 package com.alanmazzolenis.mongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.alanmazzolenis.mongodb.dto.AuthorDTO;
+import com.alanmazzolenis.mongodb.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -19,6 +22,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -72,6 +77,14 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -88,9 +101,4 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	
-	
-	
-
 }
