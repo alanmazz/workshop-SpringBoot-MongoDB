a86898e941b0d9dadc76ac6637e5cf3645ca93aa
Alan Mazzolenis <alangalvaogg@gmail.com>
2022-03-25 10:45:12 -0300

    Author data projection with DTO

--- src/main/java/com/alanmazzolenis/mongodb/config/Instantiation.java
+++ src/main/java/com/alanmazzolenis/mongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.alanmazzolenis.mongodb.domain.Post;
 import com.alanmazzolenis.mongodb.domain.User;
+import com.alanmazzolenis.mongodb.dto.AuthorDTO;
 import com.alanmazzolenis.mongodb.repository.PostRepository;
 import com.alanmazzolenis.mongodb.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acorder feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acorder feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
